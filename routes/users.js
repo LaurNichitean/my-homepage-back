c9cd0883f143928d4c4e95b6ae110ab2564450a7
Laur Nichitean <nichitean at gmail com>
2015-10-14 00:59:00 +0300

    added POST, PUT and DELETE endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var ObjectId = require('mongodb').ObjectID;
 
 /*
  * GET userlist.
@@ -9,6 +10,45 @@ router.get('/userlist', function (req, res) {
   db.collection('userlist').find().toArray(function (err, results) {
     res.json(results);
   });
+});
+
+/*
+ * POST to addUser.
+ */
+router.post('/addUser', function (req, res) {
+  var db = req.db;
+  db.collection('userlist').insert(req.body, function (err, result) {
+    res.send(
+      (err === null) ? {msg: ''} : {msg: 'Error adding user: ' + err}
+    )
+  })
+});
+
+/*
+ * PUT to modifyUser.
+ */
+router.put('/modifyUser/:id', function (req, res) {
+  var db = req.db;
+  db.collection('userlist').update({'_id': ObjectId(req.params.id)},
+    {$set: req.body},
+    function (err, result) {
+      res.send(
+        (err === null) ? {msg: ''} : {msg: 'Error updating user: ' + err}
+      )
+    })
+});
+
+/*
+ * DELETE to deleteUser.
+ */
+router.delete('/deleteUser/:id', function (req, res) {
+  var db = req.db;
+  db.collection('userlist').remove({'_id': ObjectId(req.params.id)},
+    function (err, result) {
+      res.send(
+        (err === null) ? {msg: ''} : {msg: 'Error deleting user: ' + err}
+      )
+    })
 });
 
 module.exports = router;
