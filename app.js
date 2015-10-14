959159c91f1e043ca81abaeacf9c1fe4a02c7f71
sidona <sidona_g@yahoo.com>
2015-10-14 12:44:40 +0300

    added cors support

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -21,7 +22,6 @@ var users = require('./routes/users');
 var movies = require('./routes/movies');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -33,6 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // make our database accesible to our router
 app.use(function (req, res, next) {
